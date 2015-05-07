b38cceefe97b86ec33af351c3528d25496d8967b
ISHII Tsuubito <ishii@buffer-renaiss.com>
2015-05-07 16:00:40 +0900

    add ofMatrix3x3/4x4, ofQuaternion

--- src/ofxSerializer.h
+++ src/ofxSerializer.h
@@ -315,6 +315,9 @@ inline void jsonize(std::ostream &os) const { \
     os << "}"; \
 };
 
+DefineSerializeFunction(ofMatrix3x3, mat, mat.a, mat.b, mat.c, mat.d, mat.e, mat.f, mat.g, mat.h, mat.i);
+DefineSerializeFunction(ofMatrix4x4, mat, mat._mat);
+DefineSerializeFunction(ofQuaternion, quat, quat._v)
 DefineSerializeFunction(ofVec2f, p, p.x, p.y);
 DefineSerializeFunction(ofVec3f, p, p.x, p.y, p.z);
 DefineSerializeFunction(ofVec4f, p, p.x, p.y, p.z, p.w);
